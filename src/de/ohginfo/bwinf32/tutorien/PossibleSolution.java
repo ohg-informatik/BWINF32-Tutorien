bf090c13dddbeea71f28e336ad85c9db949bd976
StruckiXxX <murat.macintosh@web.de>
2013-11-24 14:36:25 +0100

    Its finally working!

--- src/de/ohginfo/bwinf32/tutorien/PossibleSolution.java
+++ src/de/ohginfo/bwinf32/tutorien/PossibleSolution.java
@@ -9,21 +9,34 @@ public class PossibleSolution {
 	public PossibleSolution(boolean[][] dates, ArrayList<Integer> termine) {
 		this.dates = dates;
 		this.termine = termine;
+		for(int i=0;i<6;i++){
+			for(int j=0;j<6;j++){
+				System.out.print(dates[i][j] + "  ");
+			}
+			System.out.println();
+		}
+		for(int i=0;i<termine.size();i++){
+			System.out.print((termine.get(i)+1)  + "   ");
+		}
+		System.out.println();
+		System.out.println();
 	}
 	
 	public boolean testPossibility(){
-		for(int i=0;i<6;i++){
+		for(int i=0;i<termine.size();i++){
 			int count = 0;
 			for(int j=0;j<dates.length;j++){
 				for(int k=0;k<termine.size();k++){
-					if(k!=i){
+					if(j!=i){
 						if(dates[termine.get(k)][j]){
 							count++;
+							System.out.println(count);
 							break;
 						}
 					}
 				}
 			}
+			System.out.println();
 			if(count<4){
 				return false;
 			}
