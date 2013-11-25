447bc24ecfdaa1c382d5788b70e5b4ea54fcf113
JWhy <jwhy@jwhy.de>
2013-11-25 16:49:55 +0100

    Add promised start.bat
    Signed-off-by: JWhy <jwhy@jwhy.de>

---
+++ start.bat
@@ -0,0 +1,19 @@
+@ECHO OFF
+ECHO.
+ECHO.
+ECHO ============================
+ECHO ====  BWINF32-Tutorien  ====
+ECHO ====     Team  0x01     ====
+ECHO ============================
+
+cd bin
+
+ECHO.
+ECHO GUI wird gestartet. Bitte schliessen Sie dieses
+ECHO Konsolenfenster zum Schliessen des Programms.
+@java de.ohginfo.bwinf32.tutorien.Tutorien
+
+ECHO.
+ECHO Programm wurde auf Wunsch des Benutzers beendet.
+ECHO .
+PAUSE
